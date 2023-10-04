bda589ce99925f05b58375a31a855e833e1ee813
verenregina <145526870+verenregina@users.noreply.github.com>
2023-10-04 13:12:37 +0700

    Add files via upload

---
+++ ContohVariabel29.java
@@ -0,0 +1,16 @@
+public class ContohVariabel29 {
+    public static void main (String[]args){
+        String hobby = "Bermain Petak Umpet";
+        boolean isPandai = true;
+        char jeniskelamin = 'L';
+        byte umur = 20;
+        double ipk = 3.24, tinggi = 1.78;
+
+        System.out.println ("Hobby saya adalah "+hobby);
+        System.out.println ("apakah saya pandai? "+ isPandai);
+        System.out.println ("jenis kelamin : " + jeniskelamin);
+        System.out.println ("umurku saat ini : " +umur);
+        System.out.println (String.format("saya beripk "+ipk+", dengan tinggi badan "+tinggi));
+
+    }
+}
